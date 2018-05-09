ba6acd137a3269aa9380b6f5d8edceb3b1d992be
MilanAjtic <milanajtic@gmail.com>
2018-05-10 00:39:59 +0200

    promenio malo config fajlove, sad npr. da bi se doslo na login stranu ne mora da se ide na localhost/etf_partneri/index.php/Gost/login vec je dovoljno samo localhost/etf_partneri/Gost/login

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/etf_partneri';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------

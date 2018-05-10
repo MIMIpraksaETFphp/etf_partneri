2f34f9984c47fb9d6616585d03cda37261076871
MareAlas <markovicm.1403@gmail.com>
2018-05-10 14:48:16 +0200

    login bez logouta

--- application/controllers/Gost.php
+++ application/controllers/Gost.php
@@ -4,6 +4,10 @@
 class Gost extends CI_Controller {
 public function __construct() {
     parent::__construct();
+    $this->load->model("ModelKorisnik");
+    $this->load->library('session');
+    if(($this->session->userdata('korisnik'))!=NULL)
+        redirect ("Korisnik");
 }
 
 public function index(){
@@ -11,11 +15,32 @@ public function index(){
     
 }
 
-public function login($porika=NULL){
+public function login($poruka=NULL){
     $podaci=array();
     if($poruka)
         $podaci['poruka']=$poruka;
-    $this->load->view($podaci, "login.php");
+    $this->load->view("login.php");
+}
+
+public function ulogujse(){
+    $this->form_validation->set_rules("username", "Username", "required");
+    $this->form_validation->set_rules("password", "Password", "required");
+    $this->form_validation->set_message("required", "Polje {field} je ostalo prazno");
+    if($this->form_validation->run()){
+        $this->ModelKorisnik->username= $this->input->post('username');
+        if(!$this->ModelKorisnik->proveraUsername())
+            $this->login ("Neispravan username");
+            elseif (!$this->ModelKorisnik->proveraPassword($this->input->post('password')))
+                $this->login ("Neispravan password");
+            else {
+                $this->load->library('session');
+                $this->session->set_userdata('korisnik', $this->ModelKorisnik);
+                redirect("Korisnik/index");
+            }
+            
+        
+    } else
+        $this->login();
 }
 
 public function registracija(){

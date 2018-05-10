1311849421b96c130a5732fab0dec698c06a6be6
MareAlas <markovicm.1403@gmail.com>
2018-05-10 15:15:45 +0200

    logout link

---
+++ application/views/partneriClanovi.php
@@ -0,0 +1,5 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <a href="<?php echo site_url("Korisnik/logout"); ?>">Logout</a>
--- application/views/sabloni/header_gost.php
+++ application/views/sabloni/header_gost.php
@@ -29,7 +29,7 @@
           <li class="nav-item">
             <a class="nav-link" href="<?php echo site_url("Gost/login");?>">Login</a>
           </li>
-          
+
         </ul>
        
       </div>

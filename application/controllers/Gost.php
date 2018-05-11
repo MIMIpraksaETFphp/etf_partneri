b08b60598def3337545f8c9f2a83c1a5ff901606
ivanmarkovic1402 <ivan.markovic1402@gmail.com>
2018-05-11 14:03:40 +0200

    login radi...bar za sad...za ivani username

--- application/controllers/Gost.php
+++ application/controllers/Gost.php
@@ -8,11 +8,11 @@ class Gost extends CI_Controller {
         $this->load->model("ModelKorisnik");
         $this->load->library('session');
         if (($this->session->userdata('korisnik')) != NULL)
-             if ($korisnik->status_korisnika == 1)
+             if ($korisnik->status_korisnika_idtable1 == 1)
                     redirect("Korisnik/index");
-                elseif ($korisnik->status_korisnik == 2)
+                elseif ($korisnik->status_korisnika_idtable1== 2)
                     redirect("ITmenadzer");
-                elseif ($korisnik->status_korisnika == 3)
+                elseif ($korisnik->status_korisnika_idtable1== 3)
                     redirect("Admin");
                 else
                     redirect("Gost");
@@ -40,23 +40,23 @@ class Gost extends CI_Controller {
         $this->form_validation->set_rules("password", "Password", "required");
         $this->form_validation->set_message("required", "Polje {field} je ostalo prazno");
         if ($this->form_validation->run()) {
-            $this->ModelKorisnik->proveraUsername($this->input->post('username'));
+            //$this->ModelKorisnik->proveraUsername($this->input->post('username'));
             if (!$this->ModelKorisnik->proveraUsername($this->input->post('username')))
                 $this->login("Neispravan username");
             elseif (!$this->ModelKorisnik->proveraPassword($this->input->post('password')))
                 $this->login("Neispravan password");
             else {
-                /*$korisnik = $this->ModelKorisnik->korisnik;
+                $korisnik = $this->ModelKorisnik->korisnik;
                 $this->load->library('session');
                 $this->session->set_userdata('korisnik', $korisnik);
-                if ($korisnik->status_korisnika == 1)*/
+                if ($korisnik->status_korisnika_idtable1== 1)
                     redirect("Korisnik/index");
-                /*elseif ($korisnik->status_korisnik == 2)
+                elseif ($korisnik->status_korisnika_idtable1== 2)
                     redirect("ITmenadzer");
-                elseif ($korisnik->status_korisnika == 3)
+                elseif ($korisnik->status_korisnika_idtable1== 3)
                     redirect("Admin");
                 else
-                    redirect("Gost");*/
+                    redirect("Gost");
             }
         } else
             $this->login();
--- application/models/ModelKorisnik.php
+++ application/models/ModelKorisnik.php
@@ -1,16 +1,17 @@
 <?php
 
 class ModelKorisnik extends CI_Model {
-    public $username;
-    public $ime;
-    public $prezime;
-    public $idKorisnik;
+    //public $username;
+    //public $ime;
+    //public $prezime;
+    //public $idKorisnik;
     public $korisnik;
 
 
     public function __construct() {
         parent::__construct();
         $this->load->database();
+        $this->korisnik=NULL;
     }
     
        
@@ -28,19 +29,14 @@ class ModelKorisnik extends CI_Model {
         }
     }
     
-    public function proveraPassword($username,$password) {  //ovde je neka greska
-        $this->db->where('username', $username);    //ili ovde
-        $this->db->where('password', $password);
-        $result= $this->db->get('korisnik');
-        $korisnik=$result->row_array();
-        
-        if($korisnik!=NULL){
-            $this->ime=$korisnik['ime'];
-            $this->prezime=$korisnik['prezime'];
-            $this->idKorisnik=$korisnik['idKorisnik'];
+    public function proveraPassword($password) {  
+   
+        if ($this->korisnik->password== $password) {
             return TRUE;
-        }else{
+        } else {
             return FALSE;
         }
     }
+    
+    
 }

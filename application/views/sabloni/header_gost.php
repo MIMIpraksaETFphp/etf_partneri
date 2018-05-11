e4fd21e516feaf147036d0f7ba4c48c9fef83118
ivanovicivan1980 <ivanovicivan1980@gmail.com>
2018-05-11 14:12:20 +0200

    naslov

--- application/views/sabloni/header_gost.php
+++ application/views/sabloni/header_gost.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
  
-    <title>Starter Template for Bootstrap</title>
+    <title>ETF Partneri</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 
   </head>

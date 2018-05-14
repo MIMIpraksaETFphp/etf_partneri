94ecbbbc5ecbb79d6cb33f9e1a084e60590039db
MareAlas <markovicm.1403@gmail.com>
2018-05-14 15:02:35 +0200

    Predavanja

--- application/views/predavanja.php
+++ application/views/predavanja.php
@@ -1,2 +1,15 @@
 <?php
-echo "predavanja";
+
+echo "Spisak predavanja na Elektrotehnickom fakultetu u Beogradu:<br/><br/><br/>";
+
+foreach ($predavanja as $predavanje) {
+
+    echo "Naslov:<br/>" . $predavanje['naslov_srpski'] . "<br/>";
+    echo "Opis predavanja:<br/>" . $predavanje['opis_srpski'] . "<br/>";
+    echo "Sala:" . $predavanje['sala'] . "<br/>";
+    echo "Vreme predavanja:" . $predavanje['vreme_predavanja'] . "<br/>";
+    echo "Ime predavaca:" . $predavanje['ime_predavaca'] . "<br/>";
+    echo "Prezime predavaca:" . $predavanje['prezime_predavaca'] . "<br/>";
+    echo "Biografija predavaca:<br/>" . $predavanje['cv_srpski'] . "<br/>";
+    echo "<br/><br/><br/>";
+}

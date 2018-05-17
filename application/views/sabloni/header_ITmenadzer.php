a7d7ca6d9e386b70b7da6ec5fc5d593a33f9d0e7
ivanmarkovic1402 <ivan.markovic1402@gmail.com>
2018-05-17 16:53:02 +0200

    hjhj

--- application/views/sabloni/header_ITmenadzer.php
+++ application/views/sabloni/header_ITmenadzer.php
@@ -5,7 +5,7 @@
  
     <title>ETF Partneri</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-
+<!--    <link href="../../assets/css/timeline2.css" rel="stylesheet">-->
   </head>
 
   <body>
---
+++ application/views/timeline2.php
@@ -0,0 +1,56 @@
+<style>
+.container {
+    background:none;
+    width:80%;
+    margin:0 auto;
+    overflow:auto;
+    padding:2%;
+}
+.right-content, .left-content {
+    float:left;
+    display:block;
+    width:40%;
+    padding:1% 2%;
+}
+.left-content {
+    border-right:10px dotted #666;   
+    text-align:right;
+}
+.left-content p {
+    margin:150px 0 0;
+}
+.right-content p {
+    margin:80px 0 150px;
+}
+.container p {
+-webkit-transition: all 0.7s ease-in-out;
+-moz-transition: all 0.7 ease-in-out;
+-ms-transition: all 0.7s ease-in-out;
+-o-transition: all 0.7s ease-in-out;
+transition: all 0.7s ease-in-out;   
+}
+.left-content .more-padding {
+  padding-right:100px;
+}
+.right-content .more-padding {
+  padding-left:100px;
+}
+</style>
+    <div class="container">
+               <div class="left-content">
+                  <p>Content 01</p>
+                  <p>Content 02</p>
+                  <p>Content 03</p>
+                  <p>Content 04</p>
+                  <p>Content 05</p>
+               </div>
+               <div class="right-content">
+                  <p>Content 01</p>
+                  <p>Content 02</p>
+                  <p>Content 03</p>
+                  <p>Content 04</p>
+                  <p>Content 05</p>
+               </div>
+            </div>
+
+

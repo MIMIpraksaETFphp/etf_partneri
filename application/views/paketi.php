13ea730b91f6533cc44f61d1fd4c3cb9a47f4317
MareAlas <markovicm.1403@gmail.com>
2018-05-29 14:37:50 +0200

    paketi

--- application/views/paketi.php
+++ application/views/paketi.php
@@ -2,29 +2,41 @@
 if (isset($paketi)) {
     foreach ($paketi as $paket) {
         ?>
-        <table>
-            <tr>
-                <th><a href="#<?php echo $paket['naziv_paketa']; ?>"><?php echo $paket['naziv_paketa']; ?></a></th>
-                <td><?php echo $paket['vrednost_paketa']; ?></td>
-                <td><?php echo $paket['valuta']; ?></td>
-            </tr>
-        </table>
+        <div class="row">
+            <table class="table table-striped ">
+                <tr>
+                    <th class="col-sm-4"><a href="#<?php echo $paket['naziv_paketa']; ?>"><?php echo $paket['naziv_paketa']; ?></a></th>
+                    <td class="col-sm-4"><?php echo $paket['vrednost_paketa']; ?></td>
+                    <td><?php echo $paket['valuta']; ?></td>
+                </tr>
+            </table>
+        </div>
         <?php
     }
-
-    foreach ($paketi as $paket) {
-        ?>
-        <a name="<?php echo $paket['naziv_paketa']; ?>"><?php
-            echo $paket['naziv_paketa'] . "<a/><br/>";
-            $filter = array($paket['naziv_paketa']);
-            $filtriraniPaketi = array_filter($paketiStavke, function ($s) use ($filter) {
-                return in_array($s['naziv_paketa'], $filter);
-            });
-//        var_dump($filtriraniPaketi);
-            foreach ($filtriraniPaketi as $filtriraniPaket) {
-                echo $filtriraniPaket['opis'] . "<br/>";
-            }
-            echo "<br/><br/>";
-        }
-    }
     ?>
+    <table class="table table-striped ">
+        <?php
+        foreach ($paketi as $paket) {
+            ?>
+        <tr>
+                    <td><a name="<?php echo $paket['naziv_paketa']; ?>"><?php
+                            echo "<h3>".$paket['naziv_paketa'] . "</h3><a/><br/>";
+                            $filter = array($paket['naziv_paketa']);
+                            $filtriraniPaketi = array_filter($paketiStavke, function ($s) use ($filter) {
+                                return in_array($s['naziv_paketa'], $filter);
+                            });
+                            ?>
+                    </td>
+                    <td>
+                        <?php
+//        var_dump($filtriraniPaketi);
+                        foreach ($filtriraniPaketi as $filtriraniPaket) {
+                            echo $filtriraniPaket['opis'] . "<br/>";
+                        }
+                        echo "<br/><br/>";
+                    }
+                }
+                ?>
+            </td> 
+        </tr>
+</table>

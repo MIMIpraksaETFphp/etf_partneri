43a3ec8916ae77bd2801c5c6e18277223f0e23fa
MareAlas <markovicm.1403@gmail.com>
2018-06-01 14:09:56 +0200

    mail

--- application/views/mejl.php
+++ application/views/mejl.php
@@ -2,7 +2,7 @@
 <span style="background-color:red;">
     <div class="container"><!-- container class is used to centered  the body of the browser with some decent width-->
         <div class="row"><!-- row class is used for grid system in Bootstrap-->
-            <div class="col-md-4 col-md-offset-4"><!--col-md-4 is used to create the no of colums in the grid also use for medimum and large devices-->
+            <div class="col-md-8 col-md-offset-4"><!--col-md-4 is used to create the no of colums in the grid also use for medimum and large devices-->
                 <div class="login-panel panel panel-success">
                     <div class="panel-heading">
                         <h3 class="panel-title">E-mail</h3>

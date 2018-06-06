606897eb16830c483ba1dc9dc86ca905707d3db1
MilanAjtic <milanajtic@gmail.com>
2018-06-06 23:48:13 +0200

    arhiva prikazuje mejlove samo od ulogovanog korisnika

--- application/views/arhivaMejl.php
+++ application/views/arhivaMejl.php
@@ -3,31 +3,35 @@
 
 <?php
 // var_dump($mejlovi);
+// var_dump($this->session->korisnik);
 // var_dump($primaociMejla);
 foreach ($mejlovi as $mejl) {
+    if($this->session->korisnik->email == $mejl['email']){
+        ?>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-4">
+                <table class="table table-striped table-bordered">
+                    <?php
+                    echo "<tbody>";
+                    //echo "<tr><td>id mejla: </td><td>" . $mejl['idmejl'] . "</td></tr>";
+                    echo "<tr><td>from: </td><td>" . $mejl['email'] . "</td></tr>";
+                    echo "<tr><td>Ime i prezime pošiljaoca: </td><td>" . $mejl['ime'] . " " . $mejl['prezime'] . "</td></tr>";
+                    echo "<tr><td>to: </td><td>";
+                    $idMejl = $mejl['idmejl'];
+                    // var_dump($primaociMejla[$idMejl]);
+                    for ($i = 0; $i < count($primaociMejla[$idMejl]); $i++) {
+                        echo $primaociMejla[$idMejl][$i]['email_primaoca'] . ", ";
+                    }
+                    echo "</td></tr>";
+                    echo "<tr><td>vreme slanja: </td><td>" . $mejl['datum_slanja'] . "</td></tr>";
+                    echo "<tr><td>naslov mejla: </td><td>" . $mejl['naslov'] . "</td></tr>";
+                    echo "<tr><td>sadrzaj mejla: </td><td>" . $mejl['sadrzaj'] . "</td></tr><br />";
+                    echo "</tbody>";                
+                ?>
+                </table>
+            </div>
+        </div>
+    <?php
+    }
+}
     ?>
-    <div class="row">
-        <div class="col-md-8 col-md-offset-4">
-            <table class="table table-striped table-bordered">
-                <?php
-                echo "<tbody>";
-                //echo "<tr><td>id mejla: </td><td>" . $mejl['idmejl'] . "</td></tr>";
-                echo "<tr><td>from: </td><td>" . $mejl['email'] . "</td></tr>";
-                echo "<tr><td>Ime i prezime pošiljaoca: </td><td>" . $mejl['ime'] . " " . $mejl['prezime'] . "</td></tr>";
-                echo "<tr><td>to: </td><td>";
-                $idMejl = $mejl['idmejl'];
-                // var_dump($primaociMejla[$idMejl]);
-                for ($i = 0; $i < count($primaociMejla[$idMejl]); $i++) {
-                    echo $primaociMejla[$idMejl][$i]['email_primaoca'] . ", ";
-                }
-                echo "</td></tr>";
-                echo "<tr><td>vreme slanja: </td><td>" . $mejl['datum_slanja'] . "</td></tr>";
-                echo "<tr><td>naslov mejla: </td><td>" . $mejl['naslov'] . "</td></tr>";
-                echo "<tr><td>sadrzaj mejla: </td><td>" . $mejl['sadrzaj'] . "</td></tr><br />";
-                echo "</tbody>";
-            }
-            ?>
-
-        </table>
-    </div>
-</div>

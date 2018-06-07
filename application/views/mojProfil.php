d131ea56b2cf063878e1d0b112877b1fde995155
ivanmarkovic1402 <ivan.markovic1402@gmail.com>
2018-06-07 15:05:08 +0200

    moj profil

--- application/views/mojProfil.php
+++ application/views/mojProfil.php
@@ -1,6 +1,6 @@
 <br/>
 <?php 
-var_dump($partneri);
+//var_dump($partneri);
 $podaci = array(
     'ime' => $this->session->korisnik->ime,
     'prezime' => $this->session->korisnik->prezime,
@@ -21,6 +21,11 @@ $podaci = array(
     echo "<tr><td>Datum rodjenja:</td><td> " . $podaci['datum_rodjenja'] . "</td></tr>";
     echo "<tr><td>Telefon:</td><td> " . $podaci['telefon'] . "</td></tr>";
     echo "<tr><td>Email:</td><td> " . $podaci['email'] . "</td></tr>";
+    echo "<tr><td>Partneri za koje je član zadužen:</td><td> ";
+    foreach ($partneri as $partner){
+        echo "<table class='table table-striped '><tr><td>".$partner['naziv']."</td></tr></table>";
+    }
+    echo "</td></tr>";
 ?>
-<tr><td colspan='2'><a href='<?php echo site_url("$kontroler/promeniPassword");?>'>Promeni password</a></td></tr>
+<tr><td colspan='2'><a href='<?php echo site_url("$kontroler/promeniPassword");?>'><b>Promeni password</b></a></td></tr>
 </table>

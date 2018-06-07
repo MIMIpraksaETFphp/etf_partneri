2b763e561e5582a57eda4704f49c45ce6992e6a2
ivanovicivan1980 <ivanovicivan1980@gmail.com>
2018-06-07 16:21:16 +0200

    .

--- application/views/mejl.php
+++ application/views/mejl.php
@@ -1,8 +1,8 @@
 <br />
 <span style="background-color:red;">
-    <div class="container"><!-- container class is used to centered  the body of the browser with some decent width-->
-        <div class="row"><!-- row class is used for grid system in Bootstrap-->
-            <div class="col-md-8 col-md-offset-4"><!--col-md-4 is used to create the no of colums in the grid also use for medimum and large devices-->
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-4">
                 <div class="login-panel panel panel-success">
                     <div class="panel-heading">
                         <h3 class="panel-title">E-mail</h3>
@@ -12,7 +12,7 @@
                         <form role="form" method="post" action="<?php echo site_url("$kontroler/saljiMejl"); ?>">
                             <fieldset>
                                 <div class="form-group">
-                                    <input class="form-control" placeholder="TO:" name="to" type="text" value="<?php if(!empty($to)) echo $to; ?>">
+                                    <input class="form-control" placeholder="TO:" name="to" type="text" value="<?php if (!empty($to)) echo $to; ?>">
                                 </div>
                                 <?php echo form_error('to'); ?>
                                 <div class="form-group">
@@ -24,22 +24,20 @@
                                 </div>
                                 <?php echo form_error('bcc'); ?>
                                 <div class="form-group">
-                                    <input class="form-control" placeholder="Subject" name="subject" type="text" value="<?php if(!empty($subject)) echo $subject; ?>" >
+                                    <input class="form-control" placeholder="Subject" name="subject" type="text" value="<?php if (!empty($subject)) echo $subject; ?>" >
                                 </div>
-                                <?php //echo form_error('naslov'); ?>
                                 <div class="form-group">
                                     <textarea class="form-control" placeholder="Sadrzaj" name="message" id="message" type="text">
                                     </textarea>
                                     <script>
-                                        document.getElementById("message").value = "<p><?php if(!empty($message)) echo $message; ?></p>";
+                                        document.getElementById("message").value = "<p><?php if (!empty($message)) echo $message; ?></p>";
                                         CKEDITOR.replace("message");
                                     </script>
                                 </div>
                                 <input type="hidden" name="datum_slanja" value="<?php echo mdate('%Y-%m-%d %H:%i:%s', now()); ?>"/>
-                                <?php //echo form_error('sadrzaj'); ?>
-
-                                <input class="btn btn-lg btn-success btn-block" type="submit" value="Pošalji" name="submit" >
-
+                                <!--<div class="form-group">-->
+                                    <input class="btn btn-lg btn-success btn-block col-md-4" type="submit" value="Pošalji" name="submit" >
+                                <!--</div-->
                             </fieldset>
                         </form>
                     </div>

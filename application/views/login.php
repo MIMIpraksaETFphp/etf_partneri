656c2858f2bfffb933a09f288d0f5cab09418d14
ivanovicivan1980 <ivanovicivan1980@gmail.com>
2018-06-07 14:23:18 +0200

    sredjivanje login strane

--- application/views/login.php
+++ application/views/login.php
@@ -1,62 +1,31 @@
-<!--<form name="loginform" action="<?php// echo site_url('Gost/ulogujse') ?>" method="post">
-    <?php //if(isset($poruka))
-     // echo "<font color='red'>$poruka</font><br>";
-    ?>
-    Username:<input type="text" name="username" autofocus value="<?php// echo set_value('username') ?>"/>
-    <?php //echo form_error("username","<font color='red'>","</font>"); ?><br>
-    Password:<input type="password" name="password"/>
-    <?php //echo form_error("password","<font color='red'>","</font>"); ?><br>
-    <input type="submit" value="Uloguj se"/>
-</form>-->
-
-   <!-- <body>
-            <div class="container">
-                <div class="row">
-                    <div class="iffset-sm-4 col-sm-4 bg-secondary p-4 text-light">
-                        <form name="loginform" action="<?php //echo site_url('Gost/ulogujse') ?>" method="post">
-                            <div class="form-group">
-                                <label for="Username">Username:</label>
-                                <input type="text" class="form-control" name="username" id="Username"  value="<?php //echo set_value('username') ?>">
-                                <?php //echo form_error("username","<font color='red'>","</font>"); ?><br>                                     
-                            </div>
-                            <div class="form-group">
-                                <label for="Password">Password:</label>
-                                <input type="password" class="form-control" name="password" id="Password">
-                                <?php //echo form_error("password","<font color='red'>","</font>"); ?><br>
-                            </div>
-                            <input type="submit" name="submit" class="btn btn-primary" value="Uloguj se">
-                        </form>
-                    </div>
-                </div>
-            </div>-->
-        
-        
- <br/>
+<br/>
 <span style="background-color:red;">
     <div class="container">
         <div class="row">
-            <div class="col-md-4 col-md-offset-4">
+            <div class="col-md-12">
                 <div class="login-panel panel panel-success">
                     <div class="panel-heading">
-                        <h3 class="panel-title">Login</h3>
+                        <h3 class="panel-title col-md-12 col-md-offset-4">Login</h3>
                     </div>
                     <div class="panel-body">
                         <form role="form" method="post" action="<?php echo site_url('Gost/ulogujse') ?>">
                             <fieldset>
-                                 <?php
-                                if (isset($poruka))
+                                <?php
+                                if (isset($poruka)) {
                                     echo "<font color='red'>$poruka</font><br>";
+                                }
                                 ?>
                                 <div class="form-group">
-                                    <input class="form-control" placeholder="Korisnicko ime" name="username" type="text" value="<?php echo set_value('username') ?>" autofocus>
+                                    <input class="form-control col-md-4" placeholder="Korisnicko ime" name="username" type="text" value="<?php echo set_value('username') ?>" autofocus>
+                                    <span class="message col-md-8"><?php echo form_error('username'); ?></span>
                                 </div>
-                                <?php echo form_error('username'); ?>
                                 <div class="form-group">
-                                    <input class="form-control" placeholder="Lozinka" name="password" type="password" value="">
+                                    <input class="form-control col-md-4" placeholder="Lozinka" name="password" type="password" value="">
+                                    <span class="message col-md-8"><?php echo form_error('password'); ?></span>
                                 </div>
-                                <?php echo form_error('password'); ?>
-
-                                <input class="btn btn-lg btn-success btn-block" type="submit" value="Uloguj se" name="login" >
+                                <div class="form-group">
+                                    <input class="btn btn-lg btn-success btn-block col-md-2" type="submit" value="Uloguj se" name="login" >
+                                </div>
                             </fieldset>
                         </form>
                     </div>

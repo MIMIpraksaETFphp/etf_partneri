471ecfb100469dd2280d9ca404e77ab071d2a39e
MilanAjtic <milanajtic@gmail.com>
2018-06-12 00:21:19 +0200

    ...

--- application/config/config.php
+++ application/config/config.php
@@ -521,3 +521,8 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
+
+
+if(!empty(getenv("CLEARDB_DATABASE_URL"))){
+    $config['base_url'] = $_SERVER['SERVER_NAME'];
+}
